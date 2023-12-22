66662f5d7a8b2680554c497ff4e0a2790d07128d
Meriam-elyns <meriamelyns@gmail.com>
2023-12-22 13:57:20 +0100

    s

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # install puppet-lint -v 2.5.0
-exec { 'install puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint=2.5.0',
-  path    => ['/usr/bin'],
+exec { 'puppet-lint':
+command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
