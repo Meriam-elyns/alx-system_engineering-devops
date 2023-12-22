12200881f1d143532ec131d086e9bacaa09334ff
Meriam-elyns <meriamelyns@gmail.com>
2023-12-22 09:44:28 -0800

    d

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install the puppet-lint package
-package { 'puppet-lint':
+##!/usr/bin/pup
+# Using Puppt to install flask from pip3
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3'
 }
