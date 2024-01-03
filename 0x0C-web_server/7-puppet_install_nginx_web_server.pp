de21f47e51953acaec4b8d69b301a890f731f7ca
Meriam-elyns <meriamelyns@gmail.com>
2024-01-03 02:38:22 -0800

    s

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,17 @@
+# automating my work using Puppet
+package { 'ngnix':
+  ensure => installed,
+}
+file_line { 'install':
+  ensure => 'present',
+  path   => '/ect/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'newrite ^/redirect_me https://www.github.com/Meriam-elyns permanent; ',
+}
+file { '/var/www/html/index.html':
+  content =>'Hello World!',
+}
+service { 'nginx':
+  ensure => running,
+  require => package['nginx'],
+}
